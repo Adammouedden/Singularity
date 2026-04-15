4076fae8111cb2196f4433451406ef42e12b6c3c
Adam Mouedden <adam.mouedden@gmail.com>
2026-04-14 20:38:29 -0400

    Latent Reasoning | test_urm.py modified to support cpu based execution in legacy

--- reasoning/legacy/test_urm.py
+++ reasoning/legacy/test_urm.py
@@ -1,7 +1,7 @@
 import torch
 from urm.URM import URM, URMCarry
 
-device = torch.device("cuda" if torch.cuda_is.available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Replicated config from the URM paper
 config_dict = {
